199a8d26fdc46a02c34f68c23691e1360bd44ba3
Gideon Kamioka <gideonkamioka@gmail.com>
2021-08-31 00:49:58 +0900

    Delete startup.sh

--- startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-python3 -c "import openrazer.client; a = openrazer.client.DeviceManager(); a.devices[0].fx.ripple(0, 255, 0)"
-pgrep redshift | xargs kill
-redshift -l 36.2048:138.2529 -t 6500:3600 & 
-flameshot &
