65f5b568ad728506f8110dc96e4cb561b6e20670
w3zL <gideonkamioka@gmail.com>
2024-12-19 05:12:50 +0900

    adding missing nvim stuff maybe

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                ensure_installed = { 'php' },
+                highlight = {
+                    enable = true,
+                },
+                indent = {
+                    enable = true,
+                },
+            })
+        end,
+    },
+}
