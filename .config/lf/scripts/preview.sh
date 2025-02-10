fae6a1c37ed5bb30ad77f4d36f50a85959323068
w3zL <gideonkamioka@gmail.com>
2025-02-11 02:50:08 +0900

    kitten icat lf thumbnail that actually works

--- .config/lf/scripts/preview.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-file=$1
-
-case "$file" in
-    *.png |*.jpg |*.jpeg)
-        clear
-        kitten icat --stdin no --transfer-mode memory "$file" < /dev/null > /dev/tty
-    ;;
-esac
